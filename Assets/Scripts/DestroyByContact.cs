e046a49e9ab38c741cf404b61d435cfae505aac8
Kurniandha S.Y <ykurniandha@gmail.com>
2019-03-10 16:32:53 +0700

    add explosions

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Boundary")
         {
             return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            //gameController.GameOver();
         }
         Destroy(other.gameObject);
         Destroy(gameObject);
